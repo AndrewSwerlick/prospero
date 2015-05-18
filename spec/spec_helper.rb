cb1a2b7257206c554e19538ad631f625ea812474
Andrew Swerlick <andrew.swerlick@hbdi.com>
2015-05-18 01:57:00 -0400

    Remove dead code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,37 +26,5 @@ class ActiveSupport::TestCase
   end
 end
 
-
-class Minitest::Spec
-
-  # def self.let name, &block
-  #   name = name.to_s
-  #   pre, post = "let '#{name}' cannot ", ". Please use another name."
-  #   methods = Minitest::Spec.instance_methods.map(&:to_s) - %w[subject]
-  #   raise ArgumentError, "#{pre}begin with 'test'#{post}" if
-  #     name =~ /\Atest/
-  #   raise ArgumentError, "#{pre}override a method in Minitest::Spec#{post}" if
-  #     methods.include? name
-  #
-  #   mod = Module.new do
-  #     define_method name, &block
-  #   end
-  #
-  #   memo_mod = Module.new do
-  #
-  #     define_method name do
-  #       @_memoized ||= {}
-  #       @_memoized.fetch(name) do |k|
-  #         @_memoized[k] = super()
-  #       end
-  #     end
-  #   end
-  #
-  #   self.send(:include, mod)
-  #   self.send(:include, memo_mod)
-  # end
-
-end
-
 MiniTest::Spec.register_spec_type( /Controller$/,  ActionController::TestCase )
 MiniTest::Spec.register_spec_type( /^Rails::Generators/, Rails::Generators::TestCase)
