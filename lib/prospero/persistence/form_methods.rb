6d8b52629b437c24943a89553abcbabcc68b22b3
Andrew Swerlick <andrew.swerlick@hbdi.com>
2015-05-17 23:45:49 -0400

    Use anonymous module to extend form with params
    Use an anonymous module to extend the form with params method so that
    the user can override with own module declarations and super

--- lib/prospero/persistence/form_methods.rb
+++ lib/prospero/persistence/form_methods.rb
@@ -3,15 +3,20 @@ module Prospero
     module FormMethods
 
       def self.extended(base)
-        base.all_params.keys.each do |param|
-          define_method param do
-            if defined?(super)
-              super().nil? ? all_params[param] : super()
-            else
-              all_params[param]
+        mod = Module.new
+        mod.module_exec(base.all_params) do |params|
+          params.keys.each do |param|
+            define_method param do
+              if defined?(super)
+                super().nil? ? all_params[param] : super()
+              else
+                all_params[param]
+              end
             end
           end
         end
+
+        base.extend(mod)
       end
 
       def all_params
